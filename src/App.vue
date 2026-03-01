2a3c847cad84ba47266a569a1c23a4b9dc81db81
IvanRL25 <146905269+IvanRL25@users.noreply.github.com>
2026-03-01 15:43:07 -0600

    v-binding added

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <div class="cart-container">
             <div class="cart-list">
                 <div class="cart-list-item">
-                    <img src="@/assets/DragonLiver.png" alt="Dragon Liver" class="product-image">
+                    <img :src="shoppingCartItems[0].image" :alt="shoppingCartItems[0].productName" class="product-image">
                     <div class="item-details-with-actions">
                         <div class="item-details">
                             <h2>{{ shoppingCartItems[0].productName }}</h2>
@@ -16,7 +16,7 @@
                         <div class="item-actions">
                             <div class="quantity-selector">
                                 <button class="quantity-change-button">−</button>
-                                <input type="text" class="quantity-input" value="3" aria-label="quantity">
+                                <input type="text" class="quantity-input" :value="shoppingCartItems[0].quantity" aria-label="quantity">
                                 <button class="quantity-change-button">+</button>
                             </div>
                             <button class="remove-item">✕</button>
@@ -24,7 +24,7 @@
                     </div>
                 </div>
                 <div class="cart-list-item">
-                    <img src="@/assets/GoldenSnitch.png" alt="Golden Snitch" class="product-image">
+                    <img :src="shoppingCartItems[1].image" :alt="shoppingCartItems[1].productName" class="product-image">
                     <div class="item-details-with-actions">
                         <div class="item-details">
                             <h2>{{ shoppingCartItems[1].productName }}</h2>
@@ -34,7 +34,7 @@
                         <div class="item-actions">
                             <div class="quantity-selector">
                                 <button class="quantity-change-button">−</button>
-                                <input type="text" class="quantity-input" value="2" aria-label="quantity">
+                                <input type="text" class="quantity-input" :value="shoppingCartItems[1].quantity" aria-label="quantity">
                                 <button class="quantity-change-button">+</button>
                             </div>
                             <button class="remove-item">✕</button>
@@ -42,7 +42,7 @@
                     </div>
                 </div>
                 <div class="cart-list-item">
-                    <img src="@/assets/UnicornTailHair.png" alt="Unicorn Tail Hair" class="product-image">
+                    <img :src="shoppingCartItems[2].image" :alt="shoppingCartItems[2].productName" class="product-image">
                     <div class="item-details-with-actions">
                         <div class="item-details">
                             <h2>{{ shoppingCartItems[2].productName }}</h2>
@@ -52,7 +52,7 @@
                         <div class="item-actions">
                             <div class="quantity-selector">
                                 <button class="quantity-change-button">−</button>
-                                <input type="text" class="quantity-input" value="1" aria-label="quantity">
+                                <input type="text" class="quantity-input" :value="shoppingCartItems[2].quantity" aria-label="quantity">
                                 <button class="quantity-change-button">+</button>
                             </div>
                             <button class="remove-item">✕</button>
@@ -60,7 +60,7 @@
                     </div>
                 </div>
                 <div class="cart-list-item">
-                    <img src="@/assets/Wand.jpg" alt="Wand" class="product-image">
+                    <img :src="shoppingCartItems[3].image" :alt="shoppingCartItems[3].productName" class="product-image">
                     <div class="item-details-with-actions">
                         <div class="item-details">
                             <h2>{{ shoppingCartItems[3].productName }}</h2>
@@ -70,7 +70,7 @@
                         <div class="item-actions">
                             <div class="quantity-selector">
                                 <button class="quantity-change-button">−</button>
-                                <input type="text" class="quantity-input" value="1" aria-label="quantity">
+                                <input type="text" class="quantity-input" :value="shoppingCartItems[3].quantity" aria-label="quantity">
                                 <button class="quantity-change-button">+</button>
                             </div>
                             <button class="remove-item">✕</button>
@@ -78,7 +78,7 @@
                     </div>
                 </div>
                 <div class="cart-list-item">
-                    <img src="@/assets/Nimbus2000.jpg" alt="Nimbus 2000" class="product-image">
+                    <img :src="shoppingCartItems[4].image" :alt="shoppingCartItems[4].productName" class="product-image">
                     <div class="item-details-with-actions">
                         <div class="item-details">
                             <h2>{{ shoppingCartItems[4].productName }}</h2>
@@ -88,7 +88,7 @@
                         <div class="item-actions">
                             <div class="quantity-selector">
                                 <button class="quantity-change-button">−</button>
-                                <input type="text" class="quantity-input" value="1" aria-label="quantity">
+                                <input type="text" class="quantity-input" :value="shoppingCartItems[4].quantity" aria-label="quantity">
                                 <button class="quantity-change-button">+</button>
                             </div>
                             <button class="remove-item">✕</button>
@@ -98,8 +98,8 @@
             </div>
             <div class="order-summary">
                 <h2>Order summary</h2>
-                <button class="toggle-details-button">Hide Details</button>
-                <div class="">
+                <button class="toggle-details-button" @click="hideDetails = !hideDetails">{{ hideDetails ? 'Show Details' : 'Hide Details' }}</button>
+                <div :class="{'hide-order-details': hideDetails}">
                     <div class="summary-item">
                         <span>Subtotal</span>
                         <span>$13900</span>
@@ -125,6 +125,9 @@
 </template>
 
 <script setup>
+
+import {ref} from 'vue'
+
 let username = 'Harry'
 let shoppingCartItems = [
   {
@@ -168,6 +171,8 @@ let shoppingCartItems = [
     image: 'src/assets/img/Nimbus2000.jpg'
   }
 ]
+
+let hideDetails = ref(false)
 
 </script>
 
